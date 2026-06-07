dec6e50cce90a0ba37da57f01efa23a39764dc59
rajashekarpatha07 <rajashekarpatha07@gmail.com>
2026-06-07 18:54:59 +0530

    init

---
+++ index.js
@@ -0,0 +1,14 @@
+import express from "express";
+
+const app = express();
+app.use(express.json());
+
+app.get("/", async (req, res) => {
+  res.json({
+    msg: "hello from the server",
+  });
+});
+
+app.listen(2000, ()=>{
+    console.log("server is listing on post 2000")
+})
